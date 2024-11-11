fe280c5976a1cdcd2efdd72947b35df3cec0e8bd
Adib Akbari <z5488768@ad.unsw.edu.au>
2024-11-11 21:01:56 +1100

    fixed image element bug with alt showing but image box not being clickable

--- frontend/src/elements/ImageElement.jsx
+++ frontend/src/elements/ImageElement.jsx
@@ -2,25 +2,34 @@ import { Box } from '@mui/material';
 
 function ImageElement({ element, doubleClickFunc, handleClick, handleBlur, onOpenDeleteModal }) {
   return (
-    <Box
-      component="img"
-      tabIndex={-1}
-      onClick={handleClick}
-      onBlur={handleBlur}
-      onDragStart={(e) => e.preventDefault()}
-      onDoubleClick={() => doubleClickFunc(element)}
-      onContextMenu={(event) => {
-        event.preventDefault();
-        onOpenDeleteModal();
-      }}
-      sx={{
-        width: '100%',
-        height: '100%',
-      }}
-      alt={element.altText}
-      src={element.url}
-    >
-    </Box>
+    <>
+      <Box
+        tabIndex={-1}
+        onClick={handleClick}
+        onBlur={handleBlur}
+        onDragStart={(e) => e.preventDefault()}
+        onDoubleClick={() => doubleClickFunc(element)}
+        onContextMenu={(event) => {
+          event.preventDefault();
+          onOpenDeleteModal();
+        }}
+        sx={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Box
+          component="img"
+          sx={{
+            width: '100%',
+            height: '100%',
+          }}
+          alt={element.altText}
+          src={element.url}
+        >
+        </Box>
+      </Box>
+    </>
   );
 }
 
